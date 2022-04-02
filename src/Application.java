a2b92f838680b9f2c93d3afeff84cfe65c937739
BRB3D <samuraix221@hotmail.com>
2022-04-02 14:26:29 -0400

    for loop with printf and the variable i

--- src/Application.java
+++ src/Application.java
@@ -2,7 +2,13 @@ public class Application {
     public static void main(String[] args) {
 
         for(int i = 0; i < 5; i++) {
-            System.out.println("For loop");
+//            this will print the value of i and also print it in one line
+            System.out.printf("The value of i is: %d", i);
+        }
+
+        for(int i = 0; i < 3; i++) {
+//            this will print the value of i and also print it a new line thanks to the \n
+            System.out.printf("The value of i is: %d\n", i);
         }
     }
 }
