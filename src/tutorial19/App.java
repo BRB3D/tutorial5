81e70af492fee43deb3e10bceeee38f6db842ab4
BRB3D <samuraix221@hotmail.com>
2022-04-03 17:06:34 -0400

    Static variable

--- src/tutorial19/App.java
+++ src/tutorial19/App.java
@@ -1,7 +1,24 @@
 package tutorial19;
 
+class Thing {
+//    Instance variable gets its own copy
+    public String name;
+//    Static variable or class Variables only associated with the class.
+    public static String description;
+}
+
 public class App {
     public static void main(String[] args) {
+        Thing thing1 = new Thing();
+        Thing thing2 = new Thing();
 
+        Thing.description = "I am a thing";
+        System.out.println(Thing.description);
+
+        thing1.name = "Bob";
+        thing2.name = "Sue";
+
+        System.out.println(thing1.name);
+        System.out.println(thing2.name);
     }
 }
