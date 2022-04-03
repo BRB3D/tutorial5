a11e629dfaa60b49246d115b917a132787271d92
BRB3D <samuraix221@hotmail.com>
2022-04-02 23:28:29 -0400

    Comment out while loop

--- src/tutorial8/App.java
+++ src/tutorial8/App.java
@@ -7,14 +7,16 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
+//        System.out.println("Enter a number: ");
+//
+//        int value = scanner.nextInt();
+//
+//        while(value != 5) {
+//            System.out.println("Enter a number: ");
+//            value = scanner.nextInt();
+//        }
 
-        int value = scanner.nextInt();
 
-        while(value != 5) {
-            System.out.println("Enter a number: ");
-            value = scanner.nextInt();
-        }
 
         System.out.println("Got 5!");
     }
