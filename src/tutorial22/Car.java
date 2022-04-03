ab0d43f184999724f20636887a130bff9a75c99f
BRB3D <samuraix221@hotmail.com>
2022-04-03 19:06:58 -0400

    Extend class

---
+++ src/tutorial22/Car.java
@@ -0,0 +1,11 @@
+package tutorial22;
+
+public class Car extends Machine{
+
+    public void start() {
+        System.out.println("Car started");
+    }
+    public void wipeWindShield() {
+        System.out.println("Wiping windshield");
+    }
+}
