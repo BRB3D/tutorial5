42d60760c11965f5eef9b92ef8a81f41dae7b0f5
BRB3D <samuraix221@hotmail.com>
2022-04-03 16:09:37 -0400

    Setter use.

--- src/tutorial16/App.java
+++ src/tutorial16/App.java
@@ -1,14 +1,24 @@
 package tutorial16;
 
 class Robot{
-    public void speak() {
-        System.out.println("Hello");
+    public void speak(String text) {
+        System.out.println(text);
+    }
+
+    public void jump(int height) {
+        System.out.println("Jumping: " + height);
+    }
+
+    public void move(String direction, double distance){
+        System.out.println("Moving " + distance + " meter in direction " + direction);
     }
 }
 
 public class App {
     public static void main(String[] args) {
         Robot sam = new Robot();
-        sam.speak();
+        sam.speak("Hi I'm Sam.");
+        sam.jump(7);
+        sam.move("west", 12.2);
     }
 }
