27e9f9c2523e54bfae96f369ae260d2ceb9a7452
BRB3D <samuraix221@hotmail.com>
2022-04-03 23:01:57 -0400

    tutorial26 encapsulation

--- src/tutorial26/App.java
+++ src/tutorial26/App.java
@@ -1,5 +1,27 @@
 package tutorial26;
 
+class Plant {
+    public static final int ID = 7;
+    private String name;
+
+    public String getData() {
+        String data = "Some stuff " + calculateGrowthForecast();
+        return data;
+    }
+
+    private int calculateGrowthForecast() {
+        return 9;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
 public class App {
     public static void main(String[] args) {
 
